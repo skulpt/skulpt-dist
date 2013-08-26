25245526b874062b32f222b64cd20eae1d1ee196
Travis <travis@travis-ci.org>
2013-08-26 18:32:56 +0000

    Travis build 40 pushed

--- skulpt.js
+++ skulpt.js
@@ -12139,11 +12139,11 @@ Sk.builtin.nmber.prototype.numberCompare = function(other)
 }
 
 Sk.builtin.nmber.prototype.__eq__ = function(me, other) {
-	return me.numberCompare(other) == 0;
+	return (me.numberCompare(other) == 0) && !(other instanceof Sk.builtin.none);
 };
 
 Sk.builtin.nmber.prototype.__ne__ = function(me, other) {
-	return me.numberCompare(other) != 0;
+	return (me.numberCompare(other) != 0) || (other instanceof Sk.builtin.none);
 };
 
 Sk.builtin.nmber.prototype.__lt__ = function(me, other) {
@@ -12757,11 +12757,11 @@ Sk.builtin.lng.prototype.longCompare = function(other)
 }
 
 Sk.builtin.lng.prototype.__eq__ = function(me, other) {
-	return me.longCompare(other) == 0;
+	return me.longCompare(other) == 0 && !(other instanceof Sk.builtin.none);
 };
 
 Sk.builtin.lng.prototype.__ne__ = function(me, other) {
-	return me.longCompare(other) != 0;
+	return me.longCompare(other) != 0 || (other instanceof Sk.builtin.none);
 };
 
 Sk.builtin.lng.prototype.__lt__ = function(me, other) {
