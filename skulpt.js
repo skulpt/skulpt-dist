1b086d2ec5e295da990372a603a673eb196c999a
Travis <travis@travis-ci.org>
2019-07-11 13:49:42 +0000

    Travis build 1667 pushed

--- skulpt.js
+++ skulpt.js
@@ -7849,6 +7849,10 @@ function ast_for_exprStmt (c, n) {
     var varName;
     var expr1;
     var ch;
+    var ann;
+    var simple;
+    var deep;
+    var expr3;
     REQ(n, SYM.expr_stmt);
     /* expr_stmt: testlist_star_expr (annassign | augassign (yield_expr|testlist) |
                             ('=' (yield_expr|testlist_star_expr))*)
@@ -7892,8 +7896,56 @@ function ast_for_exprStmt (c, n) {
         return new Sk.astnodes.AugAssign(expr1, astForAugassign(c, CHILD(n, 1)), expr2, n.lineno, n.col_offset);
     }
     else if (CHILD(n, 1).type === SYM.annassign) {
-        // TODO translate the relevant section from ast.c
-        throw new Sk.builtin.SyntaxError("Skulpt does not yet support type assignments", c.c_filename, n.lineno);
+        if (!Sk.__future__.python3) {
+            throw new Sk.builtin.SyntaxError("Annotated assignment is not supported in Python 2", c.c_filename, n.lineno);
+        }
+        // annotated assignment
+        ch = CHILD(n, 0);
+        ann = CHILD(n, 1);
+        simple = 1;
+        deep = ch;
+        while (NCH(deep) == 1) {
+            deep = CHILD(deep, 0);
+        }
+        if (NCH(deep) > 0 && TYPE(CHILD(deep, 0)) == TOK.T_LPAR) {
+            simple = 0;
+        }
+        expr1 = ast_for_testlist(c, ch);
+        switch (expr1.constructor) {
+            case Sk.astnodes.Name:
+                varName = expr1.id;
+                forbiddenCheck(c, ch, varName, n.lineno);
+                setContext(c, expr1, Sk.astnodes.Store, ch);
+                break;
+            case Sk.astnodes.Attribute:
+                varName = expr1.attr;
+                forbiddenCheck(c, ch, varName, n.lineno);
+                setContext(c, expr1, Sk.astnodes.Store, ch);
+                break;
+            case Sk.astnodes.Subscript:
+                setContext(c, expr1, Sk.astnodes.Store, ch);
+                break;
+            case Sk.astnodes.List:
+                throw new Sk.builtin.SyntaxError("only single target (not list) can be annotated", c.c_filename, n.lineno);
+            case Sk.astnodes.Tuple:
+                throw new Sk.builtin.SyntaxError("only single target (not tuple) can be annotated", c.c_filename, n.lineno);
+            default:
+                throw new Sk.builtin.SyntaxError("illegal target for annotation", c.c_filename, n.lineno);
+        }
+        
+        if (expr1.constructor != Sk.astnodes.Name) {
+            simple = 0;
+        }
+        
+        ch = CHILD(ann, 1);
+        expr2 = ast_for_expr(c, ch);
+        if (NCH(ann) == 2) {
+            return new Sk.astnodes.AnnAssign(expr1, expr2, null, simple, n.lineno, n.col_offset);
+        } else {
+            ch = CHILD(ann, 3);
+            expr3 = ast_for_expr(c, ch);
+            return new Sk.astnodes.AnnAssign(expr1, expr2, expr3, simple, n.lineno, n.col_offset);
+        }
     }
     else {
         // normal assignment
@@ -14780,6 +14832,11 @@ Compiler.prototype.vstmt = function (s, class_for_super) {
             for (i = 0; i < n; ++i) {
                 this.vexpr(s.targets[i], val);
             }
+            break;
+        case Sk.astnodes.AnnAssign:
+            val = this.vexpr(s.value);
+            this.vexpr(s.target, val);
+            this.vexpr(s.annotation);
             break;
         case Sk.astnodes.AugAssign:
             return this.caugassign(s);
@@ -29998,6 +30055,10 @@ var DEF_FREE_CLASS = 2 << 8;
 /* free variable from class's method */
 var DEF_IMPORT = 2 << 9;
 /* assignment occurred via import */
+var DEF_NONLOCAL = 2 << 10;
+/* nonlocal stmt */
+var DEF_ANNOT = 2 << 11;
+/* this name is annotated */
 
 var DEF_BOUND = (DEF_LOCAL | DEF_PARAM | DEF_IMPORT);
 
@@ -30453,6 +30514,7 @@ SymbolTable.prototype.visitStmt = function (s) {
     var i;
     var nameslen;
     var tmp;
+    var e_name;
     Sk.asserts.assert(s !== undefined, "visitStmt called with undefined");
     switch (s.constructor) {
         case Sk.astnodes.FunctionDef:
@@ -30495,6 +30557,30 @@ SymbolTable.prototype.visitStmt = function (s) {
         case Sk.astnodes.Assign:
             this.SEQExpr(s.targets);
             this.visitExpr(s.value);
+            break;
+        case Sk.astnodes.AnnAssign:
+            if (s.target.constructor == Sk.astnodes.Name) {
+                e_name = s.target;
+                name = Sk.mangleName(this.curClass, e_name.id).v;
+                name = Sk.fixReservedNames(name);
+                cur = this.cur.symFlags[name];
+                if ((cur & (DEF_GLOBAL | DEF_NONLOCAL) )
+                    && (this.global != this.cur.symFlags) // TODO
+                    && (s.simple)) {
+                    throw new Sk.builtin.SyntaxError("annotated name '"+ name +"' can't be global", this.filename, s.lineno);
+                }
+                if (s.simple) {
+                    this.addDef(new Sk.builtin.str(name), DEF_ANNOT | DEF_LOCAL, s.lineno);
+                } else if (s.value) {
+                    this.addDef(new Sk.builtin.str(name), DEF_LOCAL, s.lineno);
+                }
+            } else {
+                this.visitExpr(s.target);
+            }
+            this.visitExpr(s.annotation);
+            if (s.value) {
+                this.visitExpr(s.value);
+            }
             break;
         case Sk.astnodes.AugAssign:
             this.visitExpr(s.target);
@@ -33656,8 +33742,8 @@ Sk.builtin.super_.__doc__ = new Sk.builtin.str(
 var Sk = {}; // jshint ignore:line
 
 Sk.build = {
-    githash: "06a1130a47dbcfbcb13b281d4d6e30bd9f9f7780",
-    date: "2019-07-10T10:06:34.604Z"
+    githash: "034074f298abbbe867bbbd1ad59be50a2f0ede2a",
+    date: "2019-07-11T13:49:13.838Z"
 };
 
 /**
